e8977309e8bcc495817958aed397fa0af9913f5b
Nilanjana28 <53992869+Nilanjana28@users.noreply.github.com>
2019-08-11 15:42:46 +0530

    Capstone Project

---
+++ Untitled.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# # Captsone Project
+# 
+
+# In[1]:
+
+
+import pandas as pd
+
+
+# In[4]:
+
+
+import numpy as np
+
+
+# In[5]:
+
+
+print ('Hello Captsone project')
+
+
+# In[ ]:
+
+
+
+
